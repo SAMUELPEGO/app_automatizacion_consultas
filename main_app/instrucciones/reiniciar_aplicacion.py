7498750174433a4340e59dd973a91bc1f05d2594
SAMUEL <samuelpego562@gmail.com>
2025-07-10 08:30:15 -0400

    updating

--- main_app/instrucciones/reiniciar_aplicacion.py
+++
@@ -1,5 +0,0 @@
-
-# INSTRUCCIONES PARA REINICIAR LA APLICACION QUE CORRESPONDEN A LA ELIMINACION DE LA BASE DE DATOS
-
-# elimina todo dentro de la carpeta migraciones
-# y elimina el archivo db.sqlite3
